c782df66bbb8f9eb94dda989be9b61f2d2c5785e
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-23 20:20:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+ apartment_creatures = {"pet" => "cat", "pest" => "cockroach", "boyfriend" => "Eric"}
 end
 
 
